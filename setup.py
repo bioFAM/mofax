a1834f8e3b4735fcf1c19c4052672f4737a12b2f
Danila Bredikhin <danila.bredikhin@embl.de>
2019-10-29 11:32:00 +0100

    Add standard Python package files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mpp-gtca",
+    version="0.0.1",
+    author="Danila Bredikhin",
+    author_email="danila.bredikhin@embl.de",
+    description="Load and interpret MODE models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gtca/mpp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
