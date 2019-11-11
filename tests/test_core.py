b0fa2bed2fbfe72a51223d91234a0e501b16e2a2
Danila Bredikhin <danila.bredikhin@embl.de>
2019-11-11 18:26:35 +0100

    Add a test for factor R2 and tearDown methods

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+import pandas as pd
 import mpp
 
 TEST_MODEL = os.path.join(os.path.dirname(__file__), 'mofa2_test_model.hdf5')
@@ -21,6 +22,21 @@ class TestMofaModelMethods(unittest.TestCase):
 
     def test_shape(self):
         self.assertIsInstance(self.model.shape, tuple)
+
+    def tearDown(self):
+        self.model.close()
+
+class TestR2(unittest.TestCase):
+
+    def setUp(self):
+        self.model = mpp.mofa_model(TEST_MODEL)
+
+    def test_factor_r2(self):
+        self.assertIsInstance(self.model.get_factor_r2(factor_index=0), pd.DataFrame)
+
+    def tearDown(self):
+        self.model.close()
+
 
 if __name__ == '__main__':
     unittest.main()
