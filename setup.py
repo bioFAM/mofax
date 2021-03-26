5706f1a95fc1d3639bb2366aa33ae7da32332eb2
Danila <32863903+gtca@users.noreply.github.com>
2021-03-26 15:46:47 +0100

    Update setup.py
    Fix version number (0.3.3)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mofax",
-    version="0.3.2",
+    version="0.3.3",
     author="Danila Bredikhin",
     author_email="danila.bredikhin@embl.de",
     description="Load and interpret MOFA models",
